f088ee601b544aa5a4f1db22ab3bbc84e068a961
Fatma YILMAZ <123100433+FatmaYlmzz@users.noreply.github.com>
2025-10-04 10:52:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-/*require('dotenv').config();
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
-
-const authRoutes = require("./routes/auth");
-const transactionRoutes = require("./routes/transactions");
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-
-// Rotalar
-app.use("/auth", authRoutes);
-app.use("/api/transactions", transactionRoutes);
-
-app.use("/dashboard", require("./routes/dashboard"));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server ${PORT} portunda çalışıyor 🚀`);
-}); son 1*/
 
 require('dotenv').config();
 console.log("JWT_SECRET:", process.env.JWT_SECRET);
@@ -43,10 +17,10 @@ app.use(bodyParser.json());
 
 // Rotalar
 app.use("/auth", authRoutes);
-app.use("/api", transactionRoutes); // artık tüm transaction route'ları /api/... altında
-app.use("/api", dashboardRoutes); // dashboard da /api/dashboard altında
+app.use("/api", transactionRoutes); 
+app.use("/api", dashboardRoutes); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server ${PORT} portunda çalışıyor 🚀`);
+  console.log(`Server ${PORT} portunda çalışıyor `);
 });
